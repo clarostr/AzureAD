1bc041255e99ad01b310b13aac89fdd783f2dbf7
clarostr <64033786+clarostr@users.noreply.github.com>
2023-08-03 12:09:54 +0300

    Update RoleDefinitions.ps1
    Added additional info regarding roles.

--- PowerShell/RoleDefinitions.ps1
+++ PowerShell/RoleDefinitions.ps1
@@ -1,5 +1,9 @@
+# A role definition is a collection of permissions. A role definition lists the operations that can be performed, such as read,
+# write, and delete. It can also list the operations that can't be performed or operations related to underlying data.
 # Owner, which has full access to all resources, including the right to delegate access to others.
 # Contributor, which can create and manage all types of Azure resources but can’t grant access to others.
 # Reader, which can view existing Azure resources.
 # By utilizing the following command we get an output regarding the role we want to check!
 Get-AzRoleDefinition Owner
+# More info on Actions, NotActions and scopes can be found on
+# https://learn.microsoft.com/en-us/training/modules/manage-users-and-groups-in-aad/5-manage-aad-roles
