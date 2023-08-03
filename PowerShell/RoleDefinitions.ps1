114a2278337b700cb211b81021681e9a9c1237c6
clarostr <64033786+clarostr@users.noreply.github.com>
2023-08-03 11:56:23 +0300

    Create RoleDefinitions.ps1
    Added Get-AzRoleDefinition

---
+++ PowerShell/RoleDefinitions.ps1
@@ -0,0 +1,5 @@
+# Owner, which has full access to all resources, including the right to delegate access to others.
+# Contributor, which can create and manage all types of Azure resources but can’t grant access to others.
+# Reader, which can view existing Azure resources.
+# By utilizing the following command we get an output regarding the role we want to check!
+Get-AzRoleDefinition Owner
